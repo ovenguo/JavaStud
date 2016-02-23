7479f96e114a7958b7de788fa087e50fcfdac9a6
ovenguo@yahoo.com <ovenguo@yahoo.com>
2016-02-23 10:56:24 +0800

    MyStudy
    my java study

---
+++ JavaStudy/src/test/Test1.java
@@ -0,0 +1,10 @@
+package test;
+import java.awt.*;
+import java.applet.*;
+import javax.swing.*;
+
+public class Test1 extends JApplet {
+	public void paint(Graphics g){
+		g.drawString("hello world", 20, 20);
+	}
+}
